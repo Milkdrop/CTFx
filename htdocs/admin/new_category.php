ccbebd9110d68a6c2228449c98e3ca9ece3a78ad
Amy <milkdrop2000@protonmail.com>
2019-08-27 18:04:29 +0200

    tiny fix

--- htdocs/admin/new_category.php
+++ htdocs/admin/new_category.php
@@ -15,8 +15,8 @@ form_input_checkbox('Exposed', true);
 form_button_submit('Create category');
 
 section_subhead ("Advanced Settings:");
-form_input_text('Available from', date_time());
-form_input_text('Available until', date_time(time() + 31536000));
+form_input_text('Available from', date_time(Config::get('MELLIVORA_CONFIG_CTF_START_TIME')));
+form_input_text('Available until', date_time(Config::get('MELLIVORA_CONFIG_CTF_END_TIME')));
 form_hidden('action', 'new');
 form_end();
 
