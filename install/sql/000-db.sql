6e1071cfd73822d23e59f1b18e83ca042755bc94
Amy <milkdrop2000@protonmail.com>
2023-02-23 17:31:06 +0000

    updated db install for latest MySQL

--- install/sql/000-db.sql
+++ install/sql/000-db.sql
@@ -1,2 +1,3 @@
 CREATE DATABASE ctfx CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-GRANT ALL PRIVILEGES ON ctfx.* TO 'ctfx'@'%' IDENTIFIED BY 'ctfx_pass';
+CREATE USER 'ctfx'@'%' IDENTIFIED BY 'ctfx_pass';
+GRANT ALL PRIVILEGES ON ctfx.* TO 'ctfx'@'%';
